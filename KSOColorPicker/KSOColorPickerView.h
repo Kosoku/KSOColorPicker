1c32094d41972f7e954c3f02d9fed60e9bd82e2a
William Towe <willbur1984@gmail.com>
2018-09-10 13:57:36 -0400

    comments

--- KSOColorPicker/KSOColorPickerView.h
+++ KSOColorPicker/KSOColorPickerView.h
@@ -17,27 +17,87 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ Enum for the color picker mode. This controls how many sliders are displayed to the user to choose components.
+ */
 typedef NS_ENUM(NSInteger, KSOColorPickerViewMode) {
+    /**
+     White is shown.
+     */
     KSOColorPickerViewModeW,
+    /**
+     White and Alpha are shown.
+     */
     KSOColorPickerViewModeWA,
+    /**
+     Red, Green, and Blue are shown.
+     */
     KSOColorPickerViewModeRGB,
+    /**
+     Red, Green, Blue, and Alpha are shown.
+     */
     KSOColorPickerViewModeRGBA,
+    /**
+     Hue, Saturation, and Brightness are shown.
+     */
     KSOColorPickerViewModeHSB,
+    /**
+     Hue, Saturation, Brightness, and Alpha are shown.
+     */
     KSOColorPickerViewModeHSBA,
+    /**
+     Convenience for the default mode.
+     */
     KSOColorPickerViewModeDefault = KSOColorPickerViewModeRGB
 };
 
+/**
+ Notification that is posted when the a KSOColorPickerView color property changes. The object of the notification is the KSOColorPickerView instance whose color property changed.
+ */
 FOUNDATION_EXTERN NSNotificationName const KSOColorPickerViewNotificationDidChangeColor;
 
+/**
+ KSOColorPickerView manages a number of subviews to facilitate picking colors by the user. It (optionally) displays the possible modes top aligned if the userCanSelectMode property is YES, followed by a swatch view that displays the selected color, and finally by a number of UISlider controls that allow the user to select the necessary color components.
+ */
 @interface KSOColorPickerView : UIView
 
+/**
+ Set and get the color displayed by the receiver.
+ 
+ The default is an appropriate color for the selected mode.
+ */
 @property (strong,nonatomic,null_resettable) UIColor *color;
 
+/**
+ Set and get the mode, which controls how many sliders are displayed to the user.
+ 
+ The default is KSOColorPickerViewModeDefault.
+ */
 @property (assign,nonatomic) KSOColorPickerViewMode mode;
+/**
+ Set and get whether the user can select the mode.
+ 
+ The default is NO.
+ */
 @property (assign,nonatomic) BOOL userCanSelectMode;
 
+/**
+ Set and get the RGB number formatter that is used to display RGB, and W values while the user is interacting with a slider.
+ 
+ The default is a number formatter configured for decimal display.
+ */
 @property (strong,nonatomic,null_resettable) NSNumberFormatter *RGBNumberFormatter;
+/**
+ Set and get the Hue number formatter that is used to display H values while the user is interacting with a slider.
+ 
+ The default is a number formatter configured for decimal display with the percent symbol (°) as a suffix.
+ */
 @property (strong,nonatomic,null_resettable) NSNumberFormatter *hueNumberFormatter;
+/**
+ Set and get the percent number formatter that is used to display SB, and A values while the user is interacting with a slider.
+ 
+ The default is a number formatter configured for percentage display.
+ */
 @property (strong,nonatomic,null_resettable) NSNumberFormatter *percentNumberFormatter;
 
 @end
